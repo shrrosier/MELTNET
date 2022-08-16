ce6d9da2658c10da43693e2b104fd926d67ff631
shrrosier <47814366+shrrosier@users.noreply.github.com>
2022-08-16 15:43:42 +0100

    Create MELTNET.m
    meltnet forward model

---
+++ MELTNET.m
@@ -0,0 +1,23 @@
+function Y = MELTNET(X,DAEnet,SEGnet,options)
+
+arguments
+    X (64,64,4,:)  % input image consisting of four bands representing bathymetry, draft, temperature and salinity
+    DAEnet SeriesNetwork % trained DAE network
+    SEGnet.params struct % weights and biases from trained segmentation net
+    SEGnet.state struct % normalization states from trained segmentation net
+    options.nClasses (1,1) = 10 % number of classes for segmentation net
+    options.clip_min (1,1) = -150 % minimum melt rate cutoff used for DAE network training
+    options.clip_max (1,1) = 10 % maximum melt rate cutoff used for DAE network training
+end 
+
+% call the segmentation network to classify melt rates
+[~, ~, ~,segnetY] = dlfeval(@lossMELTNET_SEG, SEGnet.params, X, [], options.nClasses,SEGnet.state,false,[]);
+
+% call the DAE network to convert from categorised to continuous melt rates
+DAEnetY = predict(DAEnet,segnetY);
+
+% final melt rate recovered by reversing the normalisation done to melt
+% rates in the DAE network training set
+Y = (DAEnetY./(255/(options.clip_min-options.clip_max)))+options.clip_max;
+
+end
