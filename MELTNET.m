3c006a93868700cf949c16d6096afa2ef91e7c09
shrrosier <47814366+shrrosier@users.noreply.github.com>
2022-08-17 11:50:41 +0100

    code to test meltnet

--- MELTNET.m
+++ MELTNET.m
@@ -1,17 +1,17 @@
-function Y = MELTNET(X,DAEnet,SEGnet,options)
+function Y = MELTNET(X,DAEnet,SEGparams,SEGstate,options)
 
 arguments
     X (64,64,4,:)  % input image consisting of four bands representing bathymetry, draft, temperature and salinity
     DAEnet SeriesNetwork % trained DAE network
-    SEGnet.params struct % weights and biases from trained segmentation net
-    SEGnet.state struct % normalization states from trained segmentation net
+    SEGparams struct % weights and biases from trained segmentation net
+    SEGstate struct % normalization states from trained segmentation net
     options.nClasses (1,1) = 10 % number of classes for segmentation net
     options.clip_min (1,1) = -150 % minimum melt rate cutoff used for DAE network training
     options.clip_max (1,1) = 10 % maximum melt rate cutoff used for DAE network training
 end 
 
 % call the segmentation network to classify melt rates
-[~, ~, ~,segnetY] = dlfeval(@lossMELTNET_SEG, SEGnet.params, X, [], options.nClasses,SEGnet.state,false,[]);
+[~, ~, ~,segnetY] = dlfeval(@lossMELTNET_SEG, SEGparams, X, [], options.nClasses,SEGstate,false,[]);
 
 % call the DAE network to convert from categorised to continuous melt rates
 DAEnetY = predict(DAEnet,segnetY);
---
+++ testMELTNET.m
@@ -0,0 +1,24 @@
+clearvars;
+
+load('MELTNET_DAE_C10_E500.mat','DAEnet');
+
+load('MELTNET_SEG_C10_E600.mat','params','state');
+
+input_files = dir("NClass_10/validation_inputs/*.mat");
+target_files = dir("NClass_10/validation_targets/*.png");
+nfiles = numel(input_files);
+
+X = zeros(64,64,4,nfiles);
+T = zeros(64,64,1,nfiles);
+
+for ii = 1:nfiles
+    load("NClass_10/validation_inputs/"+input_files(ii).name,'input_out');
+    img = imread("NClass_10/validation_targets/"+target_files(ii).name);
+    X(:,:,:,ii) = input_out;
+    T(:,:,:,ii) = img;
+end
+
+X = dlarray(X,'SSCB');
+
+
+MELTNET(X,DAEnet,params,state,"nClasses",10);
